4947d3e6f431bc315af0db6569c989f90f70aca3
Koji Nakagawa and Sarah Leahy <koji@>
2017-02-21 11:31:20 -0800

    create twig templates for categories and search for category ny id

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
@@ -8,13 +9,26 @@
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
+     $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+
+        return $app['twig']->render('tasks.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('tasks.html.twig', array('categories'=>Category::getAll(), 'tasks'=>Task::getAll()));
+    });
+
+    $app->get("/categories/{id}", function($id) use ($app){
+      $category = Category::find($id);
+      return $app ['twig']->render('category.html.twig', array('category'=> $category, 'tasks'=>$category->getTasks()));
     });
 
     $app->post("/tasks", function() use ($app) {
